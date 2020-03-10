5c13e492ea818b09e5c19e3dc7f78dd3082a8d91
Rafs96 <rafaelmeds28@gmail.com>
2020-03-10 15:14:36 -0300

    Colocando a imagem de fundo no app

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -3,41 +3,57 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text("Pessoas: 0",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-            ),),
-          Row(
+          Image.asset(
+            "images/restaurante.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child : FlatButton(
-                  child: Text(
-                    "+1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
-                  onPressed: (){},
-                ),
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child : FlatButton(
-                  child: Text(
-                    "-1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
-                  onPressed: (){},
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode Entra",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
               ),
             ],
-          ),
-          Text("Pode Entra",
-            style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0
-            ),),
+          )
         ],
-      )));
+      )
+    )
+  );
 }
